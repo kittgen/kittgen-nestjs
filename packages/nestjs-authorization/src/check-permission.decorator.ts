141b9811c6bf1e6d0ecf72c784636377bc9672d9
Otto von Wesendonk <ottovonwesendonk@gmail.com>
2021-01-10 23:42:55 +0100

    chore: renamed decorator and minor changes

---
+++ packages/nestjs-authorization/src/check-permission.decorator.ts
@@ -0,0 +1,7 @@
+import { applyDecorators, UseGuards } from '@nestjs/common';
+import { Action } from './action';
+import { PermissionGuard } from './permission.guard';
+
+export function CheckPermission(actions: Action[]) {
+  return applyDecorators(UseGuards(PermissionGuard(actions)));
+}
