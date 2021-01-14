93731a8c7fb140bdc76f143b473109456222a484
Edgar Mueller <edgar.m.mueller@gmail.com>
2021-01-15 00:20:29 +0100

    fix: use permission provider const in example

--- example-apps/my-nest-project/src/app.module.ts
+++ example-apps/my-nest-project/src/app.module.ts
@@ -1,6 +1,9 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthorizationModule } from '@kittgen/nestjs-authorization';
+import {
+  AuthorizationModule,
+  PERMISSION_PROVIDER,
+} from '@kittgen/nestjs-authorization';
 import { HttpsRedirectMiddleware } from '@kittgen/nestjs-https-redirect';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -15,7 +18,7 @@ import { InMemoryPermissionProvider } from './in-memory-permission.provider';
     AppService,
     IsAuthor,
     {
-      provide: 'PERMISSION_PROVIDER',
+      provide: PERMISSION_PROVIDER,
       useClass: InMemoryPermissionProvider,
     },
     {
