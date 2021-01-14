2ae1f786aaac0ecef0dc44c07dc494b82cde0c7c
Otto von Wesendonk <ottovonwesendonk@gmail.com>
2021-01-14 21:59:16 +0100

    feat: add extractors for params and query (#14)

--- packages/nestjs-authorization/src/action.ts
+++ packages/nestjs-authorization/src/action.ts
@@ -5,7 +5,7 @@ export class ActionBuilder {
   constructor(
     readonly name: string,
     public condition?: ((dto: any) => boolean) | Type<Condition>
-  ) {}
+  ) { }
 
   if(
     condition: ((ctx: ExecutionContext) => boolean) | Type<Condition>
@@ -20,5 +20,13 @@ export const CreateAction = (name: string) => new ActionBuilder(name);
 export const body = (predicate: (body: any) => boolean) => (
   ctx: ExecutionContext
 ) => predicate(ctx.switchToHttp().getRequest().body);
+
+export const params = (predicate: (params: any) => boolean) => (
+  ctx: ExecutionContext
+) => predicate(ctx.switchToHttp().getRequest().params);
+
+export const query = (predicate: (query: any) => boolean) => (
+  ctx: ExecutionContext
+) => predicate(ctx.switchToHttp().getRequest().query);
 
 export type Action = string | ActionBuilder;
