f29abaf114c425ebb40c578b035abcdb4e200f8c
Otto von Wesendonk <ottovonwesendonk@gmail.com>
2021-01-14 19:52:37 +0100

    added Permission Provider

--- packages/nestjs-authorization/src/permission.provider.ts
+++ packages/nestjs-authorization/src/permission.provider.ts
@@ -1,5 +1,7 @@
 import { PermissionSet } from './permission-set';
 
+export const PERMISSION_PROVIDER = 'PERMISSION_PROVIDER'
+
 export interface PermissionProvider {
   getPermissionSet(ctx: any): Promise<PermissionSet>;
 }
