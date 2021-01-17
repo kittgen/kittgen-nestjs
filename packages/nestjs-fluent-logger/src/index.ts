15c505cbb64f027cb2e7d0716ebc41abf88ff7df
Otto von Wesendonk <ottovonwesendonk@gmail.com>
2021-01-17 21:53:50 +0100

    fixed export

--- packages/nestjs-fluent-logger/src/index.ts
+++ packages/nestjs-fluent-logger/src/index.ts
@@ -1,1 +1,2 @@
-// add your exports here
+export { FluentLogger, FluentLoggerOptions, FluentLogBuilder } from './fluent.logger'
+export { fully, half, firstHalf, lastHalf, emailLocal, emailLocalHalf, emailDomain, emailDomainHalf } from './redaction'
