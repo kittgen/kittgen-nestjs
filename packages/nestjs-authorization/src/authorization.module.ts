11d869d942c0de8803e02366576e3e0309285118
Edgar Mueller <edgar.m.mueller@gmail.com>
2021-01-19 00:50:40 +0100

    refactor: provide async module options

--- packages/nestjs-authorization/src/authorization.module.ts
+++ packages/nestjs-authorization/src/authorization.module.ts
@@ -1,53 +1,71 @@
-import {
-  Abstract,
-  DynamicModule,
-  ForwardReference,
-  Module,
-  Type,
-} from '@nestjs/common';
+import { DynamicModule, Module, Provider, Type } from '@nestjs/common';
 import { PermissionService } from './permission.service';
 import { ConditionService } from './condition.service';
-import { PermissionProvider, PERMISSION_PROVIDER } from './permission.provider';
-import { AuthorizationModuleOptions } from './authorization-module-options.interface';
-
-@Module({})
+import {
+  AuthorizationModuleAsyncOptions,
+  AuthorizationOptionsFactory,
+} from './interfaces/authorization-module.interface';
+import { AUTHORIZATION_MODULE_OPTIONS } from './authorization.constants';
+@Module({
+  providers: [ConditionService, PermissionService],
+  exports: [ConditionService, PermissionService],
+})
 export class AuthorizationModule {
-  static register({ useClass }: AuthorizationModuleOptions): DynamicModule {
+  static registerAsync(
+    options: AuthorizationModuleAsyncOptions
+  ): DynamicModule {
+    const asyncProviders = this.createAsyncProviders(options);
     return {
+      global: options.isGlobal,
       module: AuthorizationModule,
+      imports: [...(options.imports || [])],
       providers: [
+        ...asyncProviders,
         ConditionService,
         PermissionService,
-        useClass,
-        {
-          provide: PERMISSION_PROVIDER,
-          useClass,
-        },
+        ...(options.extraProviders || []),
       ],
-      exports: [ConditionService, PermissionService, PERMISSION_PROVIDER],
+      exports: [ConditionService, PermissionService, ...asyncProviders],
     };
   }
 
-  static registerAsync(options: {
-    imports?: Array<
-      Type<any> | DynamicModule | Promise<DynamicModule> | ForwardReference
-    >;
-    inject?: Array<Type<any> | string | symbol | Abstract<any> | Function>;
-    useFactory: (
-      ...args: any
-    ) => PermissionProvider | Promise<PermissionProvider>;
-  }) {
-    const provider = {
-      provide: PERMISSION_PROVIDER,
-      useFactory: options.useFactory,
-      inject: options.inject || [],
-    };
+  /**
+   * Returns the asynchrnous providers depending on the given module
+   * options
+   * @param options Options for the asynchrnous terminus module
+   */
+  private static createAsyncProviders(
+    options: AuthorizationModuleAsyncOptions
+  ): Provider[] {
+    if (options.useFactory || options.useExisting) {
+      return [this.createAsyncOptionsProvider(options)];
+    }
+    const useClass = options.useClass as Type<AuthorizationOptionsFactory>;
+    return [
+      this.createAsyncOptionsProvider(options),
+      {
+        provide: useClass,
+        useClass,
+        inject: [...(options.inject || [])],
+      },
+    ];
+  }
 
+  private static createAsyncOptionsProvider(
+    options: AuthorizationModuleAsyncOptions
+  ): Provider {
+    if (options.useFactory) {
+      return {
+        provide: AUTHORIZATION_MODULE_OPTIONS,
+        useFactory: options.useFactory,
+        inject: options.inject || [],
+      };
+    }
     return {
-      imports: [...(options.imports || [])],
-      module: AuthorizationModule,
-      providers: [ConditionService, PermissionService, provider],
-      exports: [ConditionService, PermissionService, PERMISSION_PROVIDER],
+      provide: AUTHORIZATION_MODULE_OPTIONS,
+      useFactory: async (optionsFactory: AuthorizationOptionsFactory) =>
+        optionsFactory.createOptions(),
+      inject: [(options.useExisting || options.useClass)!],
     };
   }
 }
---
+++ packages/nestjs-authorization/src/interfaces/authorization-module.interface.ts
@@ -0,0 +1,23 @@
+import { Abstract, ModuleMetadata, Provider, Type } from '@nestjs/common';
+import { PermissionProvider } from '../permission.provider';
+
+export interface AuthorizationOptionsFactory {
+  createOptions(): Promise<AuthorizationOptions> | AuthorizationOptions;
+}
+
+export interface AuthorizationOptions {
+  permissionProvider: PermissionProvider;
+  isGlobal?: boolean;
+}
+
+export interface AuthorizationModuleAsyncOptions
+  extends Pick<ModuleMetadata, 'imports'> {
+  isGlobal?: boolean;
+  inject?: Array<Type<any> | string | symbol | Abstract<any> | Function>;
+  extraProviders?: Provider[];
+  useExisting?: Type<AuthorizationOptionsFactory>;
+  useClass?: Type<AuthorizationOptionsFactory>;
+  useFactory?: (
+    ...args: any
+  ) => AuthorizationOptions | Promise<AuthorizationOptions>;
+}
