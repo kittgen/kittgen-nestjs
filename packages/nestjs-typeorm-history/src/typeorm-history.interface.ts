8f3fd45fcc3955edf622a342f25e871415296870
Edgar Mueller <edgar.m.mueller@gmail.com>
2021-01-26 19:38:53 +0100

    fix(typeorm-hist): add missing types

--- packages/nestjs-typeorm-history/src/typeorm-history.interface.ts
+++ packages/nestjs-typeorm-history/src/typeorm-history.interface.ts
@@ -1,3 +1,5 @@
+import { EntityManager, EntitySubscriberInterface } from 'typeorm';
+
 export interface HistoryEntityMapping {
   entity: Function;
   history: Function;
@@ -5,4 +7,35 @@ export interface HistoryEntityMapping {
 
 export interface TypeOrmHistoryModuleOptions {
   entities: HistoryEntityMapping[];
+}
+
+export interface History<E> {
+  payload: E;
+  action: HistoryActionKind;
+}
+
+export interface HistoryEntitySubscriberInterface<E, H>
+  extends EntitySubscriberInterface<E> {
+  entity: Function;
+  historyEntity: Function;
+
+  createHistoryEntity(manager: EntityManager, entity: E): H | Promise<H>;
+
+  beforeInsertHistory(history: H): H | Promise<H>;
+
+  afterInsertHistory(history: H): void | Promise<void>;
+
+  beforeUpdateHistory(history: H): H | Promise<H>;
+
+  afterUpdateHistory(history: H): void | Promise<void>;
+
+  beforeRemoveHistory(history: H): H | Promise<H>;
+
+  afterRemoveHistory(history: H): void | Promise<void>;
+}
+
+export enum HistoryActionKind {
+  Created = 'CREATED',
+  Updated = 'UPDATED',
+  Deleted = 'DELETED',
 }
